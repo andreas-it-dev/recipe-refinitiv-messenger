888a4e9ed6219f3a4cb196e7aee15ab2de73815a
Andreas <awunder@alpha-it-solutions.de>
2020-12-19 04:05:02 -0500

    fixed unreads

--- webview.js
+++ webview.js
@@ -5,7 +5,7 @@ module.exports = Franz => {
     let directMessages = 0;
     unreads = document.querySelectorAll('.badge.unread-count')
     unreads.forEach(unread => directMessages += Number(unread.textContent))
-    Franz.setBadge(directMessages / 2, directMessages / 2);
+    Franz.setBadge(directMessages);
   };
 
   Franz.loop(getMessages);
